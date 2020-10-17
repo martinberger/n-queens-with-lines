ec46c9a5989c1c6e887805338c8404a540af352f
Martin Berger <M.F.Berger@sussex.ac.uk>
2020-10-17 21:41:06 +0100

    Fixed a bug regarding string equality.

--- Config.java
+++ Config.java
@@ -8,30 +8,33 @@ class Config {
     public boolean visualise = false;
     public int n = -1;    
 
-
     static void checkConsistency ( Config config ) throws CustomException {
 	if ( config.n < 0 ) {
-	    throw new CustomException ( "Need a number n to run program." );
+	    throw new CustomException ( "Problem: Need a number n >= 0 to run program." );
 	}
     }
     
     static Config parse ( String[] args ) throws CustomException {
 	var config = new Config();
-	for ( int i = 0; i < args.length; ) {
-	    if ( args [ i ] == "-visualise" ) { config.visualise = true; i++; continue; }
-	    if ( i < args.length-1 && args [ i ] == "-n" ) {
-		    i++;
-		    try {
-			config.n = Integer.parseInt( args [ i ] );
-			i++;
-		    }
-		    catch ( Exception e ) {
-			throw new CustomException ( "Could not convert numeric parameter" );
-		    }
+	for ( int i = 0; i < args.length; i++) {
+	    if ( args [ i ].equals( "-visualise" ) ) {
+		config.visualise = true;
+	    }
+	    else if ( (i < args.length-1) && (args [ i ].equals( "-n" )) ) {
+		i++;
+		try {
+		    config.n = Integer.parseInt( args [ i ] );
+		}
+		catch ( Exception e ) {
+		    throw new CustomException ( "Problem: Could not convert numeric parameter" );
+		}
+	    }
+	    else {
+		throw new CustomException ( "Problem: unknown command line format." );
 	    }
 	}
 	return config;
-    }
+    } 
 
     static Config handleCommandLine ( String[] args ) throws CustomException {
 	var config = parse ( args );
