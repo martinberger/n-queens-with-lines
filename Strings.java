cf120de3c85ae47af9be0c6ebcdbaa83c8e675cf
Martin Berger <M.F.Berger@sussex.ac.uk>
2020-10-17 20:54:13 +0100

    Added a very basic help message.

---
+++ Strings.java
@@ -0,0 +1,9 @@
+class Strings {
+   static String help =
+	"This program has the following options.\n" +
+	"\n" +
+	"    -n <int>    This parameter is required and determines board size.\n" +
+	"\n" +
+	"    -visualise  This optional parameter prints out all found valid boards.\n" +
+	"\n";
+}
