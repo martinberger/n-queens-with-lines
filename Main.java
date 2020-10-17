7cd4e2a87b4b3de1370f8e96f0d897b98636e938
Martin Berger <M.F.Berger@sussex.ac.uk>
2020-10-17 19:17:02 +0100

    The eagle has landed: code Passes planned verification.

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 class Main {
 
+    interface Predicate<T> {
+	Boolean check (T t);
+    }
+    
+    public static <T> ArrayList<T> filter ( ArrayList<T> l, Predicate<T> pred ) {
+	var res = new ArrayList<T>();
+	for ( var t : l ) {
+	    if ( !pred.check ( t ) ) {
+		res.add ( t );
+	    }
+	}
+	return res;
+    }
+    
     public static void main(String[] args) {
 	var low = Integer.parseInt( args [ 0 ] );
 	var high = Integer.parseInt( args [ 1 ] );
@@ -10,10 +26,15 @@ class Main {
         System.out.println("          7 -> 40, 8 -> 92, 9 -> 352, 10 -> 724, 11 -> 2680, 12 -> 14200, 13 -> 73712,");
 
 	for ( int n = low; n <= high; n++ ){
-	    var results = Algorithm.queens ( n );
+	    System.out.println ( "---------------------- n = " + n + " ----------------------");
+	    var results = Algorithm.queens ( n );	
+	    var resultsFiltered = filter ( results, ( ArrayList<Queen> solution ) -> Algorithm.has3Line ( solution ) );
 	    var resultsNo3Lines = Algorithm.queensNo3Lines ( n );
-	    System.out.println( "queens( " + n + " ) has " + results.size( ) + " solutions");
-	    System.out.println( "queensNo3Lines( " + n + " ) has " + resultsNo3Lines.size( ) + " solutions");	    
+	    //	    Helpers.present ( n, resultsNo3Lines );
+	    //	    System.out.println ( "      ***********" );
+	    //	    Helpers.present ( n, resultsFiltered );	    
+	    System.out.println( "queensNo3Lines( " + n + " ) has " + resultsNo3Lines.size( ) + " solutions");
+	    System.out.println( "FILTEREDqueens( " + n + " ) has " + resultsFiltered.size( ) + " solutions");	    
 	}
     }
 
