e54e17d9f7d528f65798d22ef07c1eacfb3cf0f1
Martin Berger <M.F.Berger@sussex.ac.uk>
2020-10-17 21:48:38 +0100

    Removed debugging method.

--- Queen.java
+++ Queen.java
@@ -2,5 +2,4 @@ class Queen {
     public int x;
     public int y;
     public Queen ( int x_, int y_ ) { x = x_; y = y_; }
-    public String toString() { return "(" + x + ", " + y + ")"; } // To help with debugging
 }
